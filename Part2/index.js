6e5fc65fa0d4d99c4323c2b88db69f5f7e9bafb4
unknown <wallacekid1996@gmail.com>
2021-03-21 17:01:43 -0600

    resubmit attempt

--- Part2/index.js
+++ Part2/index.js
@@ -2,9 +2,18 @@ console.log('The house always wins')
 const idInput = document.getElementById('idInput');
 const colorInput = document.getElementById('colorInput');
 
- function setCard(){
-const card = document.getElementById(idInput.value)
-card.style.color = colorInput.value
- }
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
+function resetCard(){
+   const diamonds = document.getElementById('diamonds')
+   const clubs = document.getElementById('clubs')
+   const hearts = document.getElementById('hearts')
+   const spades = document.getElementById('spades')
 
- 
+   diamonds.style.color = 'gray'
+   clubs.style.color = 'gray'
+   hearts.style.color = 'gray'
+   spades.style.color = 'gray'
+} 
